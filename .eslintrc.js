polkadot-js/dev

eslint config & swap dev to eslint (#218)
* Start config

* Bump deps

* eslint running

* ts files, eslint cleanups

.eslintrc.js
@@ -2,5 +2,4 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-module.exports = require('@polkadot/dev/config/jest');
+module.exports = require('@polkadot/dev-react/config/eslint');

packages/dev-react/config/eslint.js
@@ -2,4 +2,24 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-module.exports = require('@polkadot/dev/config/eslint');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const base = require('@polkadot/dev/config/eslint');
+
+// we are doing some magic here - with eslint 5.x we would simply do
+//   extends: ['@polkadot/dev/config/eslint', 'plugin:react/recommended']
+// however in 6.x this doesn't quite seem to work.
+module.exports = {
+  ...base,
+  extends: base.extends.concat([
+    'plugin:react/recommended'
+  ]),
+  plugins: base.plugins.concat([
+    'react-hooks'
+  ]),
+  settings: {
+    ...base.settings,
+    react: {
+      version: 'detect'
+    }
+  }
+};

plugblockchain/dev.js

.eslintrc.js
@@ -2,5 +2,4 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-module.exports = require('@plugnet/dev/config/jest');
+module.exports = require('@plugnet/dev-react/config/eslint');

packages/dev-react/config/eslint.js
@@ -2,4 +2,24 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-module.exports = require('@polkadot/dev/config/eslint');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const base = require('@plugnet/dev/config/eslint');
+
+// we are doing some magic here - with eslint 5.x we would simply do
+//   extends: ['@polkadot/dev/config/eslint', 'plugin:react/recommended']
+// however in 6.x this doesn't quite seem to work.
+module.exports = {
+  ...base,
+  extends: base.extends.concat([
+    'plugin:react/recommended'
+  ]),
+  plugins: base.plugins.concat([
+    'react-hooks'
+  ]),
+  settings: {
+    ...base.settings,
+    react: {
+      version: 'detect'
+    }
+  }
+};
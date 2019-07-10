71155b51d9b80e46190e723d5b096f313f2f6611
Jaco Greeff <jacogr@gmail.com>
2019-07-10 10:09:10 +0200

    eslint config & swap dev to eslint (#218)
    * Start config
    
    * Bump deps
    
    * eslint running
    
    * ts files, eslint cleanups

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,4 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-module.exports = require('@plugnet/dev/config/jest');
+module.exports = require('@plugnet/dev-react/config/eslint');
